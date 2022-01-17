814c9b8b2b5a7c23b3535202b73d349944d25645
Vadim Demydenko <zv090471dvv@gmail.com>
2022-01-17 14:12:43 +0300

    asd
    Signed-off-by: Vadim Demydenko <zv090471dvv@gmail.com>

---
+++ zadanie10.js
@@ -0,0 +1,16 @@
+var arr = [6,5,4,3,2,1];
+
+for (let i = 5; i >  0; i--){
+            for (let k = 0; k < i; k++){
+                if (arr[k] >  arr[k+1]) {
+                    let tmp = arr[k];
+                    arr[k] = arr[k + 1];
+                    arr[k + 1] = tmp;
+                }
+            }
+        }
+        console.log('Отсортированный массив:')    
+        for(let i = 0; i < arr.length; i++){
+                console.log(arr[i]);
+            }
+            
