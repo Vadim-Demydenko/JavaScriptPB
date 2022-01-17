c606b15341a05cce580165592f3b823e34257d31
Vadim Demydenko <zv090471dvv@gmail.com>
2022-01-17 14:00:14 +0300

    asd
    Signed-off-by: Vadim Demydenko <zv090471dvv@gmail.com>

---
+++ zadanie9.js
@@ -0,0 +1,15 @@
+var arr = [1,2,3,4,5,6];
+
+for (let i = 5; i >  0; i--){
+            for (let k = 0; k < i; k++){
+                if (arr[k] <  arr[k+1]) {
+                    let tmp = arr[k];
+                    arr[k] = arr[k + 1];
+                    arr[k + 1] = tmp;
+                }
+            }
+        }
+            for(let i = 0; i < arr.length; i++){
+                console.log(arr[i]);
+            }
+            console.log('rere');
