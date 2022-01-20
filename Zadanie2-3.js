16e11960f2569baa45d7714d3b75494a677b6e43
Vadim-Demydenko <92310303+Vadim-Demydenko@users.noreply.github.com>
2022-01-20 10:58:35 +0200

    Create Zadanie2-3.js

---
+++ Zadanie2-3.js
@@ -0,0 +1,9 @@
+function f(a, b, c){
+  if ((typeof a === 'number')&&(typeof b === 'number')&&(typeof c === 'number')){
+    return ((a - b)/c)
+  }else {
+    console.log('Аргумент должен быть числом')
+    return;
+  } 
+}
+console.log(f(-12,'sdds',2));
